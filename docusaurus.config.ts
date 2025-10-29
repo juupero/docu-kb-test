a359afe4a4787a484c131baca6b3de2395736102
Jussi Ojala <juupero@gmail.com>
2025-10-29 10:44:25 +0200

    Provide write permissions for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://juupero.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docu-kb-test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'juupero', // Usually your GitHub org/user name.
+  projectName: 'docu-kb-test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
